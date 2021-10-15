a13573d19e1e32dc2d8a075aed3fdeb95bd33842
rmalviya <ritesh.riteshmalviya.malviya@gmail.com>
2021-10-15 12:52:31 +0530

    Upgrade to null safety

--- lib/yt_player.dart
+++ lib/yt_player.dart
@@ -1,15 +1,14 @@
 import 'dart:async';
 
-import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class YtPlayer {
   static const MethodChannel _channel =
       const MethodChannel('com.owliverse/yt_player');
 
-  static Future<bool> playYouTubeVideo({
-    @required String apiKey,
-    @required String videoId,
+  static Future<bool?> playYouTubeVideo({
+    required String apiKey,
+    required String videoId,
     bool autoPlay = false,
   }) {
     return _channel.invokeMethod('playYouTubeVideo', {
